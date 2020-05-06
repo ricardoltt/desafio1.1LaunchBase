836354e7a12103c0a693c3e4668751fe34001a83
Ricardo Lopes Temoteo <ricardo.ltemoteo@gmail.com>
2020-05-06 14:05:21 -0300

    Desafio 1.2 concluído!

--- desafio1.2Objetos.js
+++ desafio1.2Objetos.js
@@ -2,29 +2,15 @@
 Crie um programa que armazena dados da empresa Rocketseat dentro de um objeto chamado empresa. Os dados a serem armazenados são:
 
 Nome: Rocketseat
-
 Cor: Roxo
-
 Foco: Programação
-
 Endereço:
-
 Rua: Rua Guilherme Gembala
 Número: 260
-Obs.: Para armazenar os dados de endereço da empresa você pode criar objetos intercalados, por exemplo:
 
-const usuario = {
-  nome: "Diego",
-  empresa: {
-    nome: "Rocketseat"
-  }
-};
 Imprima em tela utilizando console.log o nome da empresa e seu endereço no seguinte formato:
 
-A empresa Rocketseat está localizada em Rua Guilherme Gembala, 260
-Obs. Para imprimir em tela utilize o formato de template strings, por exemplo
-
-console.log(`O nome do usuário é ${usuario.nome}`);*/
+A empresa Rocketseat está localizada em Rua Guilherme Gembala, 260 */
 
 const Rocketseat = {
     nome: "Rocketseat",
--- desafio1.2Vetores.js
+++ desafio1.2Vetores.js
@@ -4,17 +4,6 @@ Um programador pode trabalhar com várias tecnologias, por isso armazene essas t
 
 As tecnologias também devem ser objetos contendo nome e especialidade, use as tecnologias abaixo:
 
-{ nome: 'C++', especialidade: 'Desktop' }
-{ nome: 'Python', especialidade: 'Data Science' }
-{ nome: 'JavaScript', especialidade: 'Web/Mobile' }
-Por exemplo:
-
-const objeto = {
-  propriedade: [
-    { nome: "C++", especialidade: "Desktop" },
-    { nome: "JavaScript", especialidade: "Web/Mobile" }
-  ]
-};
 Imprima em tela o nome e especialidade da primeira tecnologia que o usuário utiliza no seguinte formato:
 
 O usuário Carlos tem 32 anos e usa a tecnologia C++ com especialidade em Desktop*/
